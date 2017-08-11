3106ed8ed886f9c5413d0ec13065f6a8d12dcd4d
Chris Perfect <chris@identitii.com>
2017-08-11 13:52:24 +1000

    moved plugin implementation and register from hydra
    moved plugin implementation and register from hydra

---
+++ register.go
@@ -0,0 +1,50 @@
+// TODO put flag here for use in hydra builds
+
+package main
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/ory/fosite"
+	"github.com/ory/hydra-oracle-plugin/plugin"
+	"github.com/ory/hydra/client"
+	"github.com/ory/hydra/config"
+	"github.com/ory/hydra/jwk"
+	"github.com/ory/hydra/pkg"
+	"github.com/ory/hydra/warden/group"
+	"github.com/ory/ladon"
+	"github.com/sirupsen/logrus"
+	_ "gopkg.in/rana/ora.v4"
+)
+
+func init() {
+	//register the plugin with hydra
+	config.RegisterVendorPlugin("oracle", oracleVendorPlugin{}, nil)
+}
+
+//implements hydra/config.VendorPlugin
+type oracleVendorPlugin struct {
+}
+
+func (p oracleVendorPlugin) Connect(url string) (db *sqlx.DB, err error) {
+	return plugin.Connect(url)
+}
+
+func (p oracleVendorPlugin) NewClientManager(db *sqlx.DB, hasher fosite.Hasher) client.Manager {
+	return plugin.NewClientManager(db, hasher)
+}
+
+func (p oracleVendorPlugin) NewGroupManager(db *sqlx.DB) group.Manager {
+	return plugin.NewGroupManager(db)
+}
+
+func (p oracleVendorPlugin) NewJWKManager(db *sqlx.DB, aead *jwk.AEAD) jwk.Manager {
+	return plugin.NewJWKManager(db, aead)
+}
+
+func (p oracleVendorPlugin) NewOAuth2Manager(db *sqlx.DB, cm client.Manager, logger logrus.FieldLogger) pkg.FositeStorer {
+	return plugin.NewOAuth2Manager(db, cm, logger)
+}
+
+func (p oracleVendorPlugin) NewPolicyManager(db *sqlx.DB) ladon.Manager {
+	return plugin.NewPolicyManager(db)
+}
